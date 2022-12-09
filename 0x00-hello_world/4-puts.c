40f0625183e217cfb1d28f0f7cad9f470445f6e9
US-ma <unitedstateman@gmail.com>
2022-12-08 16:44:28 -0800

    Betty Standard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 
 
-	char my_char[] = "Programming is like building a multilingual puzzle";
+	char my_char[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(my_char);
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	long long int j;
 	long int i;
 	float f;
 
-	printf("Size of char: %zu bytes(s)\n", sizeof(char));
-	printf("Size of int:  %zu bytes(s)\n", sizeof(int));
-	printf("Size of long int: %zu  bytes(s)\n", sizeof(i));
-	printf("Size of long long int: %lu bytes(s)\n", sizeof(j));
-	printf("Size of float %lu bytes(s)\n", sizeof(f));
+	printf("Size of char: %zu bytes(s)\n", (unsigned long) sizeof(char));
+	printf("Size of int:  %zu bytes(s)\n", (unsigned long) sizeof(int));
+	printf("Size of long int: %zu  bytes(s)\n",  (unsigned long) sizeof(i));
+	printf("Size of long long int: %lu bytes(s)\n", (unsigned long) sizeof(j));
+	printf("Size of float %lu bytes(s)\n", (unsigned long) sizeof(f));
 
 	return (0);
 }
