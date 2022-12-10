9f0f046b23ff45658b587a900efd024a467a8cc2
US-ma <unitedstateman@gmail.com>
2022-12-09 18:52:56 -0800

    Betty code

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,11 @@ int main(void)
 	if (n % 10 > 5)
 	{
 	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-	} else if (ld == 0)
+	}
+	else if (n % 10 == 0)
 	{
 		printf("Last digit of %d is %d and is zero\n", n, n % 10);
 	} else
 printf("Last digit of %d is %d and is less than 6 and not zero\n", n, n % 10);
-	return (0);
+return (0);
 }
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,6 +10,6 @@ int main(void)
 
 	for (low = 'z'; low >= 'a'; low--)
 		putchar(low);
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
